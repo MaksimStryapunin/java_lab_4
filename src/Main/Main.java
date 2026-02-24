6450620aee25620bf39490fbf4c1baaca266e309
UsrMakson <166782494+UsrMakson@users.noreply.github.com>
2026-02-25 01:29:43 +0500

    003

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -7,60 +7,33 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("-----------------------------------------------------------------------------------------");
-        System.out.println("Выберите задание(1/2/3/4)");
+        System.out.println("Выберите задание(1/2/3)");
         System.out.print(": ");
         int choose_task = scanner.nextInt();
         switch (choose_task) {
             case 1: {
                 int switch_choose;
                 System.out.println("-----------------------------------------------------------------------------------------");
-                System.out.println("Выберите задачу(2/4/6/8/10)");
+                System.out.println("Выберите задачу(1/3)");
                 System.out.print(": ");
                 switch_choose = scanner.nextInt();
                 switch (switch_choose) {
-                    case 2: {
+                    case 1: {
                         int z;
                         System.out.print("Введите число x: ");
                         scanner.nextInt();
 
                         System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Результат: " );
+                        System.out.println("Результат: ");
                         break;
                     }
-                    case 4: {
+                    case 3: {
                         int x, z;
                         System.out.print("Введите число x: ");
                         x = scanner.nextInt();
 
                         System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Результат: " );
-                        break;
-                    }
-                    case 6: {
-                        int x, z;
-                        System.out.print("Введите число x: ");
-                        x = scanner.nextInt();
-
-                        System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Результат: " );
-                        break;
-                    }
-                    case 8: {
-                        int x, z;
-                        System.out.print("Введите число x: ");
-                        x = scanner.nextInt();
-
-                        System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Результат: " );
-                        break;
-                    }
-                    case 10: {
-                        int x, z;
-                        System.out.print("Введите число x: ");
-                        x = scanner.nextInt();
-
-                        System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Результат: " );
+                        System.out.println("Результат: ");
                         break;
                     }
                     default: {
@@ -71,116 +44,39 @@ public class Main {
                 break;
             }// Задача 1
             case 2: {
-                int switch_choose;
                 System.out.println("-----------------------------------------------------------------------------------------");
-                System.out.println("Выберите задачу(2/4/6/8/10)");
-                System.out.print(": ");
-                switch_choose = scanner.nextInt();
-                switch (switch_choose) {
-                    case 2: {
-                        int x, z;
-                        System.out.print("Введите число x: ");
-                        x = scanner.nextInt();
 
-                        System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Результат: " );
-                        break;
-                    }
-                    case 4: {
-                        int x, z;
-                        System.out.print("Введите число x: ");
-                        x = scanner.nextInt();
-
-                        System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Результат: " );
-                        break;
-                    }
-                    case 6: {
-                        int x, z;
-                        System.out.print("Введите число x: ");
-                        x = scanner.nextInt();
-
-                        System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Результат: " );
-                        break;
-                    }
-                    case 8: {
-                        int x, z;
-                        System.out.print("Введите число x: ");
-                        x = scanner.nextInt();
-
-                        System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Результат: " );
-                        break;
-                    }
-                    case 10: {
-                        int x, z;
-                        System.out.print("Введите число x: ");
-                        x = scanner.nextInt();
-
-                        System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Результат: " );
-                        break;
-                    }
-                    default: {
-                        System.out.println("Нет задачи в задании");
-                        break;
-                    }
-                }
+                System.out.println("-----------------------------------------------------------------------------------------");
                 break;
             }// Задача 2
             case 3: {
                 int switch_choose;
                 System.out.println("-----------------------------------------------------------------------------------------");
-                System.out.println("Выберите задачу(2/4/6/8/10)");
+                System.out.println("Выберите задачу(1/2/3/4)");
                 System.out.print(": ");
                 switch_choose = scanner.nextInt();
                 switch (switch_choose) {
-                    case 2: {
-                        int x, z;
-                        System.out.print("Введите число x: ");
-                        x = scanner.nextInt();
+                    case 1: {
 
                         System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Результат: " );
+                        break;
+                    }
+                    case 2: {
+
+                        System.out.println("-----------------------------------------------------------------------------------------");
+                        break;
+                    }
+                    case 3: {
+
+                        System.out.println("-----------------------------------------------------------------------------------------");
                         break;
                     }
                     case 4: {
-                        int x, z;
-                        System.out.print("Введите число x: ");
-                        x = scanner.nextInt();
 
                         System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Результат: " );
                         break;
                     }
-                    case 6: {
-                        int x, z;
-                        System.out.print("Введите число x: ");
-                        x = scanner.nextInt();
 
-                        System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Результат: " );
-                        break;
-                    }
-                    case 8: {
-                        int x, z;
-                        System.out.print("Введите число x: ");
-                        x = scanner.nextInt();
-
-                        System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Результат: " );
-                        break;
-                    }
-                    case 10: {
-                        int x, z;
-                        System.out.print("Введите число x: ");
-                        x = scanner.nextInt();
-
-                        System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Результат: " );
-                        break;
-                    }
                     default: {
                         System.out.println("Нет задачи в задании");
                         break;
@@ -188,68 +84,7 @@ public class Main {
                 }
                 break;
             }// Задача 3
-            case 4: {
-                int switch_choose;
-                System.out.println("-----------------------------------------------------------------------------------------");
-                System.out.println("Выберите задачу(2/4/6/8/10)");
-                System.out.print(": ");
-                switch_choose = scanner.nextInt();
-                switch (switch_choose) {
-                    case 2: {
-                        int x, z;
-                        System.out.print("Введите число x: ");
-                        x = scanner.nextInt();
 
-                        System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Результат: " );
-                        break;
-                    }
-                    case 4: {
-                        int x, z;
-                        System.out.print("Введите число x: ");
-                        x = scanner.nextInt();
-
-                        System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Результат: " );
-                        break;
-                    }
-                    case 6: {
-                        int x, z;
-                        System.out.print("Введите число x: ");
-                        x = scanner.nextInt();
-
-                        System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Результат: " );
-                        break;
-                    }
-                    case 8: {
-                        int x, z;
-                        System.out.print("Введите число x: ");
-                        x = scanner.nextInt();
-
-                        System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Результат: " );
-                        break;
-                    }
-                    case 10: {
-                        int x, z;
-                        System.out.print("Введите число x: ");
-                        x = scanner.nextInt();
-
-                        System.out.println("-----------------------------------------------------------------------------------------");
-                        System.out.println("Результат: " );
-                        break;
-                    }
-                    default: {
-                        System.out.println("Нет задачи в задании");
-                        break;
-                    }
-                }
-                break;
-            }// Задача 4
-            default: {
-                System.out.println("нет в списке");
-            }
         }
     }
 }
--- src/ru/stryapunin/Box.java
+++ src/ru/stryapunin/Box.java
@@ -1,4 +1,27 @@
 package ru.stryapunin;
 
-public class Box {
+public class Box<T> {
+    private T item;
+
+    public void put(T item) {
+        if (this.item != null) {
+            throw new IllegalStateException("No empty");
+        }
+        this.item = item;
+    }
+
+    public T get() {
+        T value = this.item;
+        this.item = null; // Важно: обнуляем ссылку
+        return value;
+    }
+
+    public boolean isFull() {
+        return this.item != null;
+    }
+
+    @Override
+    public String toString() {
+        return "Box {" + item + '}';
+    }
 }
