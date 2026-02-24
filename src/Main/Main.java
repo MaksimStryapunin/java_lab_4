ab901883cb178e37a17bf6c2d2c432ba276169bc
UsrMakson <166782494+UsrMakson@users.noreply.github.com>
2026-02-25 04:25:57 +0500

    010

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,7 +1,7 @@
 package Main;
 
 import java.util.*;
-import ru.stryapunin.*;
+import ru.stryapunin.Box;
 import ru.stryapunin.Comparable;
 
 public class Main {
@@ -20,12 +20,12 @@ public class Main {
                 switch_choose = scanner.nextInt();
                 switch (switch_choose) {
                     case 1: {
-                        Generalized_Box();
+                        generalized_Box();
                         System.out.println("-----------------------------------------------------------------------------------------");
                         break;
                     }
                     case 3: {
-                        _Comparable();
+                        comparable();
                         System.out.println("-----------------------------------------------------------------------------------------");
                         break;
                     }
@@ -38,7 +38,7 @@ public class Main {
             }// Задача 1
             case 2: {
                 System.out.println("-----------------------------------------------------------------------------------------");
-
+                maxBoxes();
                 System.out.println("-----------------------------------------------------------------------------------------");
                 break;
             }// Задача 2
@@ -77,51 +77,94 @@ public class Main {
                 break;
             }// Задача 3
         }
-
-        private static void Generalized_Box() { //Задача 1
+    }
+        private static void generalized_Box() { //Задача 1.1
             Box<Integer> box = new Box<>();
+            Scanner scanner1 = new Scanner(System.in);
             while (true) {
                 System.out.println("\n1. Положить число: ");
                 System.out.println("2. Извлечь число: ");
-                System.out.println("3. Посмотреть статус: ");
-
-                int action = input.getInt("Действие: ");
-
+                System.out.println("3. Посмотреть статус: \n");
+                int temp;
+                temp = scanner1.nextInt();
                 try {
-                    switch (action) {
-                        case 1 -> {
-                            int value = input.getInt("Введите значение: ");
+                    switch (temp) {
+                        case 1: {
+                            System.out.print("введите число: ");
+                            int value = scanner1.nextInt();
                             box.put(value);
+                            break;
                         }
-                        case 2 -> {
+                        case 2: {
                             Integer value = box.get();
                             if (value == null) {
                                 System.out.println("Коробка пуста");
                             } else {
                                 System.out.println("Извлечено значение: " + value);
                             }
+                            break;
                         }
-                        case 3 -> System.out.println("status: " + box);
-                        default -> System.out.println("Неверная команда");
+                        case 3: System.out.println("status: " + box);break;
+                        default: System.out.println("Неверная команда"); break;
                     }
                 } catch (IllegalStateException e) {
                     System.out.println("ERROR: " + e.getMessage());
                 }
             }
         }
-        private static void _Comparable() {
-            class String implements Comparable<String> {
-                private String value;
 
-                public String(String value) {
-                    this.value = value;
+        private static void comparable() { //Задача 1.3
+            Scanner scanner2 = new Scanner(System.in);
+            class string implements Comparable<String> {
+                private String x;
+
+                public string(String x) {
+                    this.x = x;
                 }
 
                 @Override
                 public int compare(String obj) {
-                    return this.value.compareTo(obj);
+                    return this.x.compareTo(obj);
+                }
+            }
+            System.out.print("введите число 1: ");
+            String n1 = scanner2.nextLine();
+            System.out.print("введите число 2: ");
+            String n2 = scanner2.nextLine();
+            string x = new string(n1);
+            int result = x.compare(n2);
+            System.out.println("Сравнение чисел: " + result);
+        }
+    private static void maxBoxes() { //Задача 2.2
+        Scanner scanner3 = new Scanner(System.in);
+        System.out.println("Введите количество коробок: ");
+        int count = scanner3.nextInt();
+        if (count <= 0) {
+            System.out.println("Количество должно быть больше чем 0");
+            return;
+        }
+
+        List<Box<? extends Number>> boxes = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            Box<Double> b = new Box<>();
+            System.out.println("Введите число для коробки " + (i + 1) + ": ");
+            double val = (double) scanner3.nextInt();
+            b.put(val);
+            boxes.add(b);
+        }
+
+        double max = Double.NEGATIVE_INFINITY;
+        for (Box<? extends Number> box : boxes) {
+            if (box.isFull()) {
+                // Извлекаем значение
+                double val = box.get().doubleValue();
+                if (val > max) {
+                    max = val;
                 }
             }
         }
+        System.out.println("Максимальное значение среди " + count + " коробок: " + max);
     }
+
 }
